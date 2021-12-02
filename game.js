efca335f2ee12a76ab628b5ca3d43f5318fc6de8
scooperl <scooperl@live.unc.edu>
2021-12-02 16:14:24 -0500

    Autoclicker, Dean Dome, Displays Added

--- game.js
+++ game.js
@@ -1,10 +1,79 @@
+// Access the game elements 
+const upgrade = document.querySelector("#upgrade");
+const rameses = document.querySelector("#rameses");
+const upgradeCostDoc = document.querySelector("#upgradeCost");
+const scoreDoc = document.querySelector("#score");
+const autoClicker = document.querySelector("#buyAutoClicker");
+const clickerCostDoc = document.querySelector("#clickerCost");
+const deanDomeDoc = document.querySelector("#deanDome");
+const deanDomeCostDoc = document.querySelector("#deanDomeCost");
 
 
-const upgrade = document.getElementById("upgrade");
-const rameses = document.getElementById("rameses");
+// Initialize the game variables 
 var level = 1;
 var score = 0;
+var upgradeCost = 25;
+var clickerCost = 500;
+var autoClickerLevel = 0; 
+var deanDome = false;
+var deanDomeCost = 1000;
 
-upgrade.onclick = e => level++;
 
-rameses.onclick = e => score+= level*1;
+// Initial display values
+scoreDoc.innerHTML = "Fever Points: " + score;
+upgradeCostDoc.innerHTML = "Upgrade Cost: " + upgradeCost;
+clickerCostDoc.innerHTML = "Auto Clicker Cost: " + clickerCost;
+
+// Upgrade cursor click function
+upgrade.onclick = function(){
+    if (score >= upgradeCost) {
+        score -= upgradeCost;
+        updateScore();
+        upgradeCost = upgradeCost*3;
+        updateCost();
+        level++;
+    }
+}
+
+// Manual Rameses click function
+rameses.onclick = function() {
+    score+= (level**2)*1;
+    updateScore();
+}
+
+// Buy auto clicker function
+autoClicker.onclick = function() {
+    if (score >= clickerCost) {
+        score -= clickerCost;
+        updateScore();
+        clickerCost = clickerCost*5;
+        updateClickerCost();
+        autoClickerLevel++;
+    }
+}
+
+
+// Increase score for auto clicker
+window.setInterval(function() {
+    score += (autoClickerLevel**2)*1;
+    if (deanDome) { score += 10; }
+    updateScore();
+}, 500);
+
+//Dean Dome Functions
+deanDomeDoc.onclick = function() {
+    if (score>= deanDomeCost) {
+        score -= deanDomeCost;
+        updateScore();
+        deanDome = true;
+        deanDomeCostDoc.remove();
+        deanDomeDoc.src = "./DeanDome.jpg";
+    } 
+}
+deanDomeDoc.onmouseover = function() { };
+
+// Update Score and Upgrade Cost Display
+function updateScore() { scoreDoc.innerHTML = "Fever Points: " + score;}
+function updateCost() { upgradeCostDoc.innerHTML = "Upgrade Cost: " + upgradeCost;}
+function updateClickerCost() {clickerCostDoc.innerHTML = "Auto Clicker Cost: " + clickerCost}
+
