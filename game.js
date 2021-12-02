ff1c8aff6c705e79bffecdb4c4bf9e3c8949abef
scooperl <scooperl@live.unc.edu>
2021-12-02 12:36:05 -0500

    Rameses clicker and upgrade added

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-const upgrade = document.querySelector("#upgrade");
+
+
+const upgrade = document.getElementById("upgrade");
+const rameses = document.getElementById("rameses");
+var level = 1;
+var score = 0;
+
+upgrade.onclick = e => level++;
+
+rameses.onclick = e => score+= level*1;
