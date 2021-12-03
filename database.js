dd271918867b32929292e8dffa1df305d736fee6
Cole Vahey <rcvahey01@gmail.com>
2021-12-02 22:31:31 -0500

    Pretty sure I just got the server totally up to date with all of the fields in game.js

--- database.js
+++ database.js
@@ -14,7 +14,7 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, user TEXT NOT NULL UNIQUE, email TEXT, pass TEXT, lastLogin TEXT, score INT );`;
+        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, user TEXT NOT NULL UNIQUE, email TEXT, pass TEXT, lastLogin TEXT, score INT, level INT, upgradeCost INT, clickerCost INT, autoClickerLevel INT, deanDome INT, deanDomeCost INT );`;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
 // Echo information about what we just did to the console.
